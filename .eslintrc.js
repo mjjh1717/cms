f3f2fb96c987d4ea35c97dfd8fb635c8f4ddd409
mjjh1717git <1692762315@qq.com>
2021-09-13 16:40:39 +0800

    feat(main): 项目配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,11 +4,11 @@ module.exports = {
     node: true
   },
   extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint",
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    '@vue/prettier',
+    '@vue/prettier/@typescript-eslint',
     'plugin:prettier/recommended'
   ],
   parserOptions: {
